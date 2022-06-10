70ce019c21c3fe9fb86bdf407ff037580165afd5
minhquan73 <quannmse151005@fpt.edu.vn>
2022-06-10 18:43:48 +0700

    first commit

---
+++ src/main/java/com/group1/TgddBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgddBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgddBackendApplication.class, args);
+	}
+
+}
