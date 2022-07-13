a9f2f591d854f768a3320bf55e777a00faccf73b
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-07-13 15:06:44 +0700

    add security config

---
+++ src/main/java/com/group1/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.group1.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.group1.service.user.CustomUserDetailService;
+import com.group1.service.user.CustomUserDetail;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailService();
+	}
+	
+	
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		
+		return authProvider;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			//.antMatchers("/list_users").authenticated()
+			.antMatchers("/ViewProduct").authenticated()
+			.antMatchers("/new").authenticated()
+			.anyRequest().permitAll()
+			.and().formLogin()
+				.usernameParameter("username")
+				.defaultSuccessUrl("/adminpage")
+				.permitAll()
+			.and().logout().logoutSuccessUrl("/").permitAll();
+	}
+}
