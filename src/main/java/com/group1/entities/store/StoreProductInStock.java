a085d0fb5cceb95da725b89a4f72762bc766862f
MaiThanhHoang <86399605+DragonKnightSamie@users.noreply.github.com>
2022-07-14 16:32:46 +0700

    Updated DB, project structure and admin feature

--- src/main/java/com/group1/entities/store/StoreProductInStock.java
+++ src/main/java/com/group1/entities/store/StoreProductInStock.java
@@ -7,8 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.group1.entities.product.Color;
 import com.group1.entities.product.Product;
 
 //mqfixed
@@ -16,39 +19,91 @@ import com.group1.entities.product.Product;
 @Table(name = "store_products_in_stock")
 public class StoreProductInStock implements Serializable {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="ID")
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "StoreID", referencedColumnName = "StoreID", insertable = false, updatable = false)
+	private Store storeIdentity;
+	
 	@Column(name = "StoreID")
-	private Store storeId;
+	private Integer storeId;
+	
+	@ManyToOne
+	@JoinColumn(name = "ProductID", referencedColumnName = "ProductID", insertable = false, updatable = false)
+	private Product productIdentity;
+	
 	@Column(name = "ProductID")
-	private Product productId;
+	private Integer productId;
+	
 	@Column(name = "Quantity")
 	private Integer quantity;
+	
 	@Column(name = "LocalPrice")
 	private Integer localPrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "ColorID", referencedColumnName = "ColorID", insertable = false, updatable = false)
+	private Color colorIdentity;
+	
+	@Column(name = "ColorID")
+	private Integer colorId;
 
 	public StoreProductInStock() {
 	}
 
-	public StoreProductInStock(Store storeId, Product productId, Integer quantity, Integer localPrice) {
+	public StoreProductInStock(Integer id, Store storeIdentity, Integer storeId, Product productIdentity,
+			Integer productId, Integer quantity, Integer localPrice, Color colorIdentity, Integer colorId) {
+		super();
+		this.id = id;
+		this.storeIdentity = storeIdentity;
 		this.storeId = storeId;
+		this.productIdentity = productIdentity;
 		this.productId = productId;
 		this.quantity = quantity;
 		this.localPrice = localPrice;
+		this.colorIdentity = colorIdentity;
+		this.colorId = colorId;
 	}
 
-	public Store getStoreId() {
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Store getStoreIdentity() {
+		return storeIdentity;
+	}
+
+	public void setStoreIdentity(Store storeIdentity) {
+		this.storeIdentity = storeIdentity;
+	}
+
+	public Integer getStoreId() {
 		return storeId;
 	}
 
-	public void setStoreId(Store storeId) {
+	public void setStoreId(Integer storeId) {
 		this.storeId = storeId;
 	}
 
-	public Product getProductId() {
+	public Product getProductIdentity() {
+		return productIdentity;
+	}
+
+	public void setProductIdentity(Product productIdentity) {
+		this.productIdentity = productIdentity;
+	}
+
+	public Integer getProductId() {
 		return productId;
 	}
 
-	public void setProductId(Product productId) {
+	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
 
@@ -67,9 +122,29 @@ public class StoreProductInStock implements Serializable {
 	public void setLocalPrice(Integer localPrice) {
 		this.localPrice = localPrice;
 	}
-	
-	
 
-	
+	public Color getColorIdentity() {
+		return colorIdentity;
+	}
+
+	public void setColorIdentity(Color colorIdentity) {
+		this.colorIdentity = colorIdentity;
+	}
+
+	public Integer getColorId() {
+		return colorId;
+	}
+
+	public void setColorId(Integer colorId) {
+		this.colorId = colorId;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + "\n       storeIdentity=" + storeIdentity + "\n       storeId=" + storeId
+				+ "\n       productIdentity=" + productIdentity + "\n       productId=" + productId
+				+ "\n       quantity=" + quantity + "\n       localPrice=" + localPrice + "\n       colorIdentity="
+				+ colorIdentity + "\n       colorId=" + colorId;
+	}
 
 }
