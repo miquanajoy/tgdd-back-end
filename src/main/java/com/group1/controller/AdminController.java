0e9367092959e341a64addbcc45a78d6fe5ad383
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-07-21 15:20:37 +0700

    viewbybrand

--- src/main/java/com/group1/controller/AdminController.java
+++ src/main/java/com/group1/controller/AdminController.java
@@ -739,33 +739,47 @@ public class AdminController {
 	
 	@CrossOrigin(origins = "*")
 	@GetMapping(value = "/products-management/view-products")
-	public List<Product> viewProducts(ModelAndView model, @RequestParam(required = false) Integer category) {
-		
-		
+	public List<Product> viewProducts(ModelAndView model, 
+			@RequestParam(name = "category", required = false) Integer category,
+			@RequestParam(name = "manufacturer", required = false) Integer manufacturer) {
+
 		List<Product> productList = null;
-		
-		if (category == null) {
-			productList = productServ.showAllProducts();
+
+		if (category != null) {
+			if (manufacturer != null) {
+				productList = productServ.findProductByManufacturer(manufacturer);
+			} else {
+				productList = productServ.findProductByCategory(category);
+			}
+
 		} else {
-			productList = productServ.findProductByCategory(category);
-			System.out.print(category);
+			productList = productServ.showAllProducts();
 		}
 
-		for(Product pro: productList) 
-		{
+		for (Product pro : productList) {
 			String encoder64 = Base64.getEncoder().encodeToString(pro.getImage());
 			pro.setImageToShow(encoder64);
 		}
 		return productList;
 	}
-	
+
 	@CrossOrigin(origins = "*")
 	@GetMapping(value = "/products-management/view-category")
 	public List<Category> viewByCategory(ModelAndView model) {
-		
+
 		List<Category> categoryList = cateServ.getAllCategorys();
 		return categoryList;
-		
+
+	}
+
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/products-management/view-manufacturer")
+	public List<Manufacturer> viewByManufacturerByCategory(ModelAndView model,
+			@RequestParam(required = true) Integer categoryId) {
+
+		List<Manufacturer> manufacturerList = manuServ.getAllCateBrands(categoryId);
+		return manufacturerList;
+
 	}
 	
 	
